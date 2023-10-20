4e8b437d00d3c4d1f2a80fc001aa27dcf013e137
wlynxg <liuauthor@foxmail.com>
2023-10-20 16:35:53 +0800

    feat: update stun server parse logic

--- cmd/stun/stun.go
+++ cmd/stun/stun.go
@@ -2,25 +2,30 @@ package stun
 
 import (
 	"log"
+	"net"
 	"net/netip"
 
 	"github.com/spf13/cobra"
 	"github.com/wlynxg/nsend/stun"
 )
 
+var (
+	port uint16
+)
+
 // Cmd represents the dns command
 var Cmd = &cobra.Command{
-	Use:   "stun",
+	Use:   "stun <STUN server>",
 	Short: "send STUN request to network hosts",
 	Long:  `stun can send STUN request to the specified host。`,
 	Run: func(cmd *cobra.Command, args []string) {
-		dst, err := netip.ParseAddrPort(args[0])
+		dst, err := net.ResolveIPAddr("ip", args[0])
 		if err != nil {
 			log.Fatalf("invalid addr: %s %v", args[0], err)
 		}
 
 		err = stun.Run(stun.Opt{
-			Server: dst,
+			Server: netip.AddrPortFrom(netip.MustParseAddr(dst.String()), port),
 		})
 		if err != nil {
 			log.Fatalln(err)
@@ -30,4 +35,5 @@ var Cmd = &cobra.Command{
 }
 
 func init() {
+	Cmd.Flags().Uint16VarP(&port, "port", "p", stun.DefaultSTUNPort, "stun server port")
 }
